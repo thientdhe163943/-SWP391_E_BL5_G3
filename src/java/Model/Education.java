e9cf072d5df7ee40ee29a7cb73b28d52c5dfe9ae
iamthanng <iamthanng@gmail.com>
2024-12-01 21:26:55 +0700

    Update model education

--- src/java/Model/Education.java
+++ src/java/Model/Education.java
@@ -11,18 +11,28 @@ package Model;
 public class Education {
     private int eduId;
     private int cvId;
-    private String university;
+    private String schoolName;
+    private String major;
+    private String description;
+    
 
     public Education() {
     }
 
-    public Education(int cvId, String university) {
+    public Education(int eduId, int cvId, String schoolName, String major, String description) {
+        this.eduId = eduId;
         this.cvId = cvId;
-        this.university = university;
+        this.schoolName = schoolName;
+        this.major = major;
+        this.description = description;
     }
 
     public int getEduId() {
         return eduId;
+    }
+
+    public void setEduId(int eduId) {
+        this.eduId = eduId;
     }
 
     public int getCvId() {
@@ -33,11 +43,27 @@ public class Education {
         this.cvId = cvId;
     }
 
-    public String getUniversity() {
-        return university;
+    public String getSchoolName() {
+        return schoolName;
     }
 
-    public void setUniversity(String university) {
-        this.university = university;
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
