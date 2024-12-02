5e50aa8adf0458c46cde9fc580b51fefd6aea57d
thientdhe163943 <thientdhe163943@fpt.edu.vn>
2024-12-02 14:08:39 +0700

    fix import

--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -2,32 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package Model;
 
 /**
  *
- * @author laptop368
+ * @author Hayashi
  */
 public class Account {
-    private int account_id;
+    private int accountId;
     private String username;
     private String password;
-
+    private int roleId;
     public Account() {
     }
 
-    public Account(int account_id, String username, String password) {
-        this.account_id = account_id;
+    public Account(int accountId, String username, String password, int roleId) {
+        this.accountId = accountId;
+        this.username = username;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+   
+
+    public Account(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public int getAccount_id() {
-        return account_id;
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
     }
 
-    public void setAccount_id(int account_id) {
-        this.account_id = account_id;
+    public int getAccountId() {
+        return accountId;
     }
 
     public String getUsername() {
@@ -44,6 +60,11 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "accountId=" + accountId + ", username=" + username + ", password=" + password + ", roleId=" + roleId + '}';
     }
     
 }
