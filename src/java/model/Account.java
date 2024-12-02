d3abfd15354463c3b91c5017d0a3f269a8a68a50
thientdhe163943 <thientdhe163943@fpt.edu.vn>
2024-12-02 14:06:47 +0700

    commit

---
+++ src/java/model/Account.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author laptop368
+ */
+public class Account {
+    private int account_id;
+    private String username;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(int account_id, String username, String password) {
+        this.account_id = account_id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
