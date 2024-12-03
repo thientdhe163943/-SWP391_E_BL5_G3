fabd64a3e820ed104ebd4e1021776e6fdaceacb0
lampthe162003 <lampthe162003@fpt.edu.vn>
2024-12-03 12:56:42 +0700

    add sorting (pending fixes)

--- src/java/controller/mentee/MenteeRequestListServlet.java
+++ src/java/controller/mentee/MenteeRequestListServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.mentee;
 
 import Dao.RequestDAO;
@@ -14,39 +13,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import Model.Request;
+import java.util.Collections;
 
 /**
  *
  * @author Hayashi
  */
 public class MenteeRequestListServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet MenteeRequestListServlet</title>");  
+            out.println("<title>Servlet MenteeRequestListServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet MenteeRequestListServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet MenteeRequestListServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,17 +57,18 @@ public class MenteeRequestListServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         RequestDAO dao = new RequestDAO();
         ArrayList<Request> requestList = dao.getAllRequests();
-        
-        request.setAttribute("requestList", requestList);
-        
-        request.getRequestDispatcher("../view/mentee/home.jsp").forward(request, response);
-    } 
 
-    /** 
+        request.setAttribute("requestList", requestList);
+
+        request.getRequestDispatcher("../view/mentee/home.jsp").forward(request, response);
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -72,8 +76,24 @@ public class MenteeRequestListServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException {
+        RequestDAO dao = new RequestDAO();
+        String sortOrder = (String) request.getParameter("sortOrder");
+        System.out.println("sortOrder");
+        ArrayList<Request> requestList = dao.getAllRequests();;
+
+        if (sortOrder == null || sortOrder.isEmpty()) {
+            request.setAttribute("requestList", requestList);
+        } else if (sortOrder.equalsIgnoreCase("title")) {
+            requestList.sort((o1, o2) -> o1.getTitle().compareTo(o2.getTitle()));
+            request.setAttribute("requestList", requestList);
+
+        } else if (sortOrder.equalsIgnoreCase("mentor")) {
+            requestList.sort((o1, o2) -> o1.getMentor().getName().compareTo(o2.getMentor().getName()));
+            request.setAttribute("requestList", requestList);
+        }
+
+        request.getRequestDispatcher("../view/mentee/home.jsp").forward(request, response);
     }
 
 }
