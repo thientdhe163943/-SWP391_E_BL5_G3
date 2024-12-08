27a21119c2b35df9f6ce0a501299efc8ac5fb2fc
lampthe162003 <lampthe162003@fpt.edu.vn>
2024-12-08 13:24:05 +0700

    change username and password of the db

--- src/java/DB/DBConnect.java
+++ src/java/DB/DBConnect.java
@@ -19,9 +19,9 @@ public class DBConnect {
     {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=HappyProgramming;encrypBookStoret=true;trustServerCertificate=true";
-            String username = "sa";
-            String password = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Bookstore;encrypBookStoret=true;trustServerCertificate=true";
+            String username = "Verliezer";
+            String password = "123456";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
