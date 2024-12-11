072b0af8c68eaacbf622e68109bd023d219d136c
lampthe162003 <lampthe162003@fpt.edu.vn>
2024-12-11 15:44:35 +0700

    new servlet for mentee update request status

---
+++ src/java/controller/mentee/MenteeUpdateRequestStatusServlet.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.mentee;
+
+import Dao.RequestDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Hayashi
+ */
+public class MenteeUpdateRequestStatusServlet extends HttpServlet {
+   
+    private final RequestDAO requestDao = new RequestDAO();
+
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int requestId = Integer.parseInt(request.getParameter("requestId"));
+        int status = Integer.parseInt(request.getParameter("status"));
+        
+        boolean isAffected = requestDao.updateStatus(requestId, status);
+        
+        response.sendRedirect("mentee");
+    }
+
+}
